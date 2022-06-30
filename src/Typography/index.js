8bca477402c4da59e3705d0d7f14d0d77fcf0632
Nick <nickd8@gmail.com>
2022-06-30 14:29:38 +0100

    index.html added

--- src/Typography/index.js
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled"
-import { color, textStyle, space, typography } from "styled-system"
-
-export const Canon = styled.h1(textStyle, color, space, typography)
-export const Trafalger = styled.h1(textStyle, color, space, typography)
-export const Testimonial = styled.h1(textStyle, color, space, typography)
-export const BodyCopy = styled.p(textStyle, color, space, typography)
